4f9484c4fca3be6c3c78359caf6e521947b200c2
David Song <songdasol@gmail.com>
2021-10-11 12:29:01 -0700

    included typeof.

--- javascript-primitives-and-variables/main.js
+++ javascript-primitives-and-variables/main.js
@@ -8,14 +8,13 @@ var totalPets = 1;
 console.log('value of totalPets:', totalPets);
 
 console.log('typeof fullName:', typeof fullName);
-
 console.log('typeof isCool:', typeof isCool);
 console.log('typeof totalPets:', typeof totalPets);
 
 var unknown = null;
-console.log('value of unknown:', unknown);
+console.log('typeof value of unknown:', unknown);
 console.log('typeof unknown:', typeof unknown);
 
 var never;
 console.log('value of never:', never);
-console.log('typeof never:', never);
+console.log('typeof never:', typeof never);
