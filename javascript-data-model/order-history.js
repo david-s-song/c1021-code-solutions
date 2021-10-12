5bba2cec9b5942476e142de9bd9333d503cd58bc
David Song <songdasol@gmail.com>
2021-10-12 15:27:52 -0700

    still working on it.

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -8,7 +8,7 @@ var orderHistory = [
     orderDetails: '',
     orderInvoice: '',
     dateDelievered: '',
-    itemName: 'JavaScript for impatient programmers',
+    item: 'JavaScript for impatient programmers',
     itemAuthor: 'Rauschmayer, R. Axel',
     beforeTaxes: 31.55,
     itemReturnedWindowClosed: 'Sep 7, 2020'
@@ -22,7 +22,7 @@ var orderHistory = [
     orderDetails: '',
     orderInvoice: '',
     dateDelievered: 'July 20, 2020',
-    itemName: 'The Timeless Way of Building',
+    item: 'The Timeless Way of Building',
     itemAuthor: 'Alexander, Christopher',
     beforeTaxes: 41.33,
     itemReturnedWindowClosed: 'Aug 19, 2020'
@@ -36,7 +36,7 @@ var orderHistory = [
     orderDetails: '',
     orderInvoice: '',
     dateDelievered: 'July 7, 2020',
-    itemName: 'Gamecube Controller Adapter',
+    item: 'Gamecube Controller Adapter',
     itemAuthor: '',
     beforeTaxes: 15.98,
     itemReturnedWindowClosed: 'Aug 19, 2020'
@@ -50,23 +50,19 @@ var orderHistory = [
     orderDetails: '',
     orderInvoice: '',
     dateDelievered: 'July 5, 2020',
-    itemName: 'Gamecube Controller',
-    itemAuthor: '',
-    beforeTaxes: 15.98,
-    itemReturnedWindowClosed: 'Aug 19, 2020'
-  },
-
-  {
-    orderNumber: '11328831772648248',
-    orderPlaced: 'July 3, 2020',
-    orderTotal: 138.93,
-    orderShipTo: 'JS Master',
-    orderDetails: '',
-    orderInvoice: '',
-    dateDelievered: 'July 5, 2020',
-    itemName: 'The Art of Sql',
-    itemAuthor: 'Faroult, Stephanie',
-    beforeTaxes: 33.99,
-    itemReturnedWindowClosed: 'Aug 19, 2020'
+    item: [
+      {
+        itemName: 'Gamecube Controller',
+        itemAuthor: '',
+        beforeTaxes: 15.98,
+        itemReturnedWindowClosed: 'Aug 19, 2020'
+      },
+      {
+        itemName: 'The Art of Sql',
+        itemAuthor: 'Faroult, Stephanie',
+        beforeTaxes: 33.99,
+        itemReturnedWindowClosed: 'Aug 19, 2020'
+      }
+    ]
   }
 ];
