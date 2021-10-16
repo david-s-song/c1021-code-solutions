988ae3e7c9c07bb8023a9992b8f2df01400ad5f2
David Song <songdasol@gmail.com>
2021-10-15 20:28:45 -0700

    followed requested instructions.

---
+++ string-manipulation-1/src/get-first-char.js
@@ -0,0 +1,24 @@
+/* exported getFirstChar */
+
+function getFirstChar(string) {
+  var firstChar = string[0];
+  return firstChar;
+}
+
+/*
+brainstorm
+1. identify the characters: string is an object.
+2. understand how index numbering works
+3. draw a diagram for index numbering - place index reference
+4. create a storage space for the first character
+5. identify the first character of the string object
+6. move the first character of string object into a storage place
+7. check to see if the next character correlates with index numbering
+position of zero
+
+Pseudo-Code
+1. create a storage place for output
+2. placing the first character of the potential word to the string
+into the empty storage unit
+3. givng the output.
+*/
