3515dc0c030da32b1e708cbe1b5c08ad437a3c8c
David Song <songdasol@gmail.com>
2021-10-19 06:36:44 -0700

    on last problem.

--- javascript-loops-practice/find-index.js
+++ javascript-loops-practice/find-index.js
@@ -1,17 +1,9 @@
 /* exported findIndex */
+
 function findIndex(array, value) {
-  var index = [];
   for (var i = 0; i < array.length; i++) {
-    array.find(value[0] = 1);
-  } return index;
+    if (array[i] === value) {
+      return i;
+    }
+  } return -1;
 }
-
-/*
-steps
-1. defined function
-2. created an empty variable
-3. using for loop (condition) to see each of the items in array using .length
-4. incrementing to check each of the items in array
-5. using find method to 'find' parameter value at 0 index and assinging it to 1...
-
-*/
--- javascript-loops-practice/odd-or-even.js
+++ javascript-loops-practice/odd-or-even.js
@@ -1,1 +1,10 @@
 /* exported oddOrEven */
+
+function oddOrEven(numbers) {
+  for (var i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 === 0) {
+      return 'even';
+    }
+  }
+  return 'odd';
+}
