5650087f1a165d1791fa5cb6b2fca899afa965fe
David Song <songdasol@gmail.com>
2021-10-20 10:28:31 -0700

    in progress.

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -1,39 +1,15 @@
-var onOff = true;
-var yellowButton = document.querySelector('.yellow-circle-button');
+
+var bulbOff = document.querySelector('.yellow-circle-button');
+var onOff = false;
 
 function isItOn() {
-  if (onOff === true) {
-    yellowButton.className = 'yellow-circle-button';
-  } else if (onOff !== true) {
-    yellowButton.className = 'black-circle-button';
+  if (onOff) {
+    bulbOff.className = 'black-circle-button';
+    onOff = false;
+  } else {
+    bulbOff.className = 'yellow-circle-button';
+    onOff = true;
   }
 }
 
-yellowButton.addEventlistener('click', isItOn);
-
-/*
-var yesOrNo = true;
-var lightOn = document.querySelector('.yellow-circle-button');
-var lightOff = document.querySelectorAll('row');
-
-function onOff() {
-  if (yesOrNo === lightOn) {
-    lightOn.className = 'yellow-circle-button';
-  } else if (yesOrNo !== lightOn) {
-    lightOff.className = 'black-circle-button';
-  }
-}
-
-lightOn.addEventListener('click', onOff);
-
-function toYellow() {
-  if (yesOrNo === true) {
-    lightOff.className = 'black-circle-button';
-  } else if (yesOrNo !== true) {
-    lightOff.className = 'yellow-circle-button';
-  }
-}
-
-lightOn.addEventListener('click', toBlack);
-lightOff.addEventListener('click', toYellow);
-*/
+bulbOff.addEventlistener('click', isItOn);
