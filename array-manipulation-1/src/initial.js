b373818b010f8aaf158542edd9720f8cb96f3def
David Song <songdasol@gmail.com>
2021-11-01 21:57:14 -0700

    completed array-manipulation-1.

--- array-manipulation-1/src/initial.js
+++ array-manipulation-1/src/initial.js
@@ -1,18 +1,15 @@
 /* exported initial */
 function initial(array) {
   var removingLastItem = [];
-  for (var i = 0; i < array.length; i++) {
-    if (i !== array.length - 1) {
-      removingLastItem.push(array[i]);
-    }
+  for (var i = 0; i < array.length - 1; i++) {
+    removingLastItem.push(array[i]);
   }
   return removingLastItem;
 }
 
 /*
 1. create a new container
-2. Check each items in the container
-3. if the item in last position in the container does not match the first item
-4.  in the new container
-5. put this container into the output
+2. Check each items in the container until the second to the last position.
+3. Place all items except the last item in the last position into the new container
+5. Put this container into the output
 */
--- array-manipulation-1/src/tail.js
+++ array-manipulation-1/src/tail.js
@@ -1,18 +1,15 @@
 /* exported tail */
 function tail(array) {
   var tailEl = [];
-  for (var i = 0; i < array.length; i++) {
-    if (i !== 0) {
-      tailEl.push(array[i]);
-    }
+  for (var i = 1; i < array.length; i++) {
+    tailEl.push(array[i]);
   }
   return tailEl;
 }
 
 /*
 1. Create a new container
-2. Check each items in the container
-3. If each item is not in the first position of the container in sequence,
-4. add the next item(s) in the new container
-5. Put this container as output
+2. Check each items in the container starting from the second position
+3. add the item(s) into the new container
+4. Put this container as output
 */
