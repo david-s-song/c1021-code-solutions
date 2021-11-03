c77a308f2220cbf12822d7ac32d12aa20379659a
David Song <songdasol@gmail.com>
2021-11-03 12:30:59 -0700

    in progress

---
+++ javascript-timers/interval.js
@@ -0,0 +1,49 @@
+
+// var $heading = document.addEventListener('load', countdown);
+// var $numbers = 4;
+
+// function countdown(event) {
+//   $numbers--;
+//   if($numbers > 0) {
+//     $heading.textContent = $numbers;
+//   } else {
+//     $heading.textContent = '~Earth Beeeelooowww Us~'
+//     clearInterval()
+//   }
+//     $header.push(i++);
+//   }
+// }
+
+// // setTimeout(changeHeader, 2.0 * 1000);
+
+// // $header.textContent = '4';
+// // $header.textContent = '3';
+// // $header.textContent = '2';
+// // $header.textContent = '1';
+// // $header.textContent = '~Earth Beeeelooowww Us~';
+
+// // var intervalID;
+
+// // function changeText () {
+// //   if (!intervalId) {
+// //     intervalID = setInterval(countdown)
+// //   }
+// // }
+
+// // = setInterval(function () {}
+
+// // document.querySelector('h1').addEventListener('load', countdown);
+
+// // function change(event) {
+// //   if ($header.TextContent === '4') {
+// //     $header = setInterval(countDown, 2000);
+// //   }
+// // }
+
+// // function countDown(event) {
+
+// // }
+
+// // function stopCount(event) {
+
+// // }
