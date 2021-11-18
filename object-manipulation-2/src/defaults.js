df5746a33e551d47d9cde32c0e45426392c7d961
David Song <songdasol@gmail.com>
2021-11-18 12:51:32 -0800

    completed object-manipulation-2.

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -1,1 +1,22 @@
 /* exported defaults */
+function defaults(target, source) {
+  for (prop in source) {
+    if (source.hasOwnProperty(prop) !== target.hasOwnProperty(prop)){
+      target[prop] = source[prop];
+    }
+  }
+}
+
+/*
+Psuedocode
+1. run through the given entity (source) and identify each of its traits
+2. if the traits of the entity (source) does not match the traits of the second given entity (target)
+3. put the traits of the entity (source) into the second given entity (target)
+
+
+
+notes:
+1. hasOwnProperty() method returns a boolean indicating
+whether the object has the specified property as its own property (as opposed to inheriting it).
+
+*/
--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -1,25 +1,27 @@
 /* exported pick */
 function pick(source, keys) {
-  var outcome = {};
-  for (properties in source) {
-    if (keys.includes(properties)) {
-      outcome[properties] = source[properties];
-    }
+  var x = {};
+  for (properties of keys) {
+    var y = source[properties];
+    if (y !== undefined)
+      x[properties] = y;
   }
-  return outcome;
+  return x;
 }
 
-pick({ foo: 1, bar: 2, baz: 3 }, ['foo', 'baz']); // -> { foo: 1, baz: 3 }
+
+
+// pick({ qux: 4, corge: 5 }, ['bar', 'grault'])    // -> {}
 
 /*
 
 PseudoCode
-1. Create a storage container to hold entities
-2. Look through the all traits of the given entity
-3. if the traits of the entity matches the entities in the storage container holding them
-4. put its traits and descriptions into the storage container
-5. put the storage container as output
+1. Create a storage container to hold entities (x)
+2. Look through the given items in the storage container (for of for arrays)
+4. put the items as traits of the given entity (source), and put this into a new container
+5. if the new container (y) does not match undefined
+6. put the traits in the given entity into the storage container (x) to hold entities
+that match traits in the new container (y)
+7. put the storage container as output
 
-
-https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...in
   */
